b8bc444ec3a7b08ff94adefd06fcc96b834b6aad
BoorBoorBoor <easy192736@gmail.com>
2026-03-04 13:53:47 +0300

    IO

--- papochka/array.py
+++ papochka/array.py
@@ -1,19 +1,24 @@
 N_MAX = 15
-arr = list()
-n = int(input("Enter number of elements (max 15): "))
-if n > N_MAX:
-    n = N_MAX
-    print("n is limited to 15")
-print("Enter elements:")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
 
-print("Array:")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
-print("")
+def read_array():
+    arr = list()
+    n = int(input("Enter number of elements: "))
+    print("Enter elements:")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+    return arr
+
+def print_array(arr):
+    n = len(arr)
+    print("Array:")
+    i = 0
+    while (i < n):
+        print(arr[i], end = " ")
+        i += 1
+    print("")
+
+my_arr = read_array()
+print_array(my_arr)
